eac9554fee4c47c32c592baa42312d18d4e493ea
Rishi Malgwa <rishimalgwa35@gmail.com>
2023-08-17 05:44:41 +0530

    fix: primary key

--- pkg/models/model.go
+++ pkg/models/model.go
@@ -8,7 +8,7 @@ import (
 )
 
 type BaseModel struct {
-	ID        uuid.UUID `gorm:"primaryKey;type:uuid"`
+	ID        uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"ID"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt *sql.NullTime `gorm:"index"`
--- pkg/models/video.go
+++ pkg/models/video.go
@@ -1,16 +1,22 @@
 package models
 
 import (
-	"time"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Video struct {
 	BaseModel
-	Name         string    `json:"name"`
-	PublishedAt  string    `json:"published_at"`
-	ThumbnailUrl string    `json:"thumbnail_url"`
-	ChannelId    string    `json:"channel_id"`
-	Description  string    `json:"description"`
-	ChannelName  string    `json:"channel_name"`
-	CreatedDate  time.Time `json:"created_date"`
+	Name             string `json:"name"`
+	PublishedAt      string `json:"published_at"`
+	ThumbnailUrl     string `json:"thumbnail_url"`
+	ChannelId        string `json:"channel_id"`
+	Description      string `json:"description"`
+	ChannelName      string `json:"channel_name"`
+	SearchDocWeights string `gorm:"column:search_doc_weights"`
+}
+
+func (v *Video) BeforeCreate(_ *gorm.DB) error {
+	v.ID = uuid.New()
+	return nil
 }
