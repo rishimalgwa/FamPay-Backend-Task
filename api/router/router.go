b75e7f84be6a0ba0c02ccb1d0375fa13090f3581
Rishi Malgwa <rishimalgwa35@gmail.com>
2023-08-17 06:46:39 +0530

    feat: video routes

--- api/router/router.go
+++ api/router/router.go
@@ -1,7 +1,13 @@
 package router
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/rishimalgwa/FamPay-Backend-Task/api/controllers"
+)
 
 func MountRoutes(c *fiber.App) {
-
+	thisController := controllers.VideoController{}
+	videoGroup := c.Group("/videos")
+	videoGroup.Get("/get", thisController.GetAllVideos)
+	videoGroup.Get("/search", thisController.SearchVideo)
 }
