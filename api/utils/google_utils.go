b3d0f88a220b774befdff4ef5d94411af89db17f
Rishi Malgwa <rishimalgwa35@gmail.com>
2023-08-17 15:38:14 +0530

    docs: commets added

--- api/utils/google_utils.go
+++ api/utils/google_utils.go
@@ -27,11 +27,13 @@ var (
 )
 
 func GetYouTubeVideos(query string) error {
-	apiKey = viper.GetString("YT_API_KEY1")
+	// Load YouTube API key from configuration
+	apiKey := viper.GetString("YT_API_KEY1")
 	if apiKey == "" {
 		log.Fatal("YOUTUBE_API_KEY environment variable not set")
 	}
 
+	// Create a background context and initialize YouTube service
 	ctx := context.Background()
 	service, err := youtube.NewService(ctx, option.WithAPIKey(apiKey))
 	if err != nil {
@@ -39,8 +41,11 @@ func GetYouTubeVideos(query string) error {
 		return err
 	}
 
+	// Define API request parts
 	parts := [2]string{"id", "snippet"}
 	var searchResponse *youtube.SearchListResponse
+
+	// Build API request based on the current state
 	if number == 0 {
 		searchResponse, err = service.Search.List(parts[:]).
 			Q(query).
@@ -53,7 +58,6 @@ func GetYouTubeVideos(query string) error {
 			MaxResults(50).PageToken(youtubeNext).
 			Type("video").
 			Do()
-
 	}
 	if err != nil {
 		log.Fatalf("Error calling YouTube Data API: %v", err)
@@ -64,15 +68,19 @@ func GetYouTubeVideos(query string) error {
 	if searchResponse.NextPageToken != "" {
 		youtubeNext = searchResponse.NextPageToken
 	}
-	for _, v := range searchResponse.Items {
 
-		saveVideos(db.GetDB(), v)
+	// Iterate over search results and save videos
+	for _, v := range searchResponse.Items {
+		if err := saveVideos(db.GetDB(), v); err != nil {
+			log.Printf("Error saving video: %v", err)
+		}
 	}
+
 	return nil
 }
 
-func saveVideos(db *gorm.DB, video *youtube.SearchResult) (*models.Video, error) {
-
+func saveVideos(db *gorm.DB, video *youtube.SearchResult) error {
+	// Prepare data to be saved
 	toBeSaved := &models.Video{
 		Name:         video.Snippet.Title,
 		Description:  video.Snippet.Description,
@@ -81,36 +89,41 @@ func saveVideos(db *gorm.DB, video *youtube.SearchResult) (*models.Video, error)
 		ChannelId:    video.Snippet.ChannelId,
 		ChannelName:  video.Snippet.PublishedAt,
 	}
-	tx := db.Begin()
 
+	// Begin a transaction
+	tx := db.Begin()
 	if err := tx.Create(&toBeSaved).Error; err != nil {
 		tx.Rollback()
-		return nil, err
+		return err
 	}
 
+	// Update search weights
 	if err := tx.Exec(`
 		UPDATE videos
 		SET search_weights = setweight(to_tsvector(name), 'A') || setweight(to_tsvector(COALESCE(description, '')), 'B')
 	`).Error; err != nil {
 		tx.Rollback()
-		return nil, err
+		return err
 	}
 
+	// Drop existing index
 	if err := tx.Exec(`DROP INDEX IF EXISTS search__weights_idx`).Error; err != nil {
 		tx.Rollback()
-		return nil, err
+		return err
 	}
 
+	// Create new GIN index
 	if err := tx.Exec(`
 		CREATE INDEX search__weights_idx ON videos USING GIN(search_weights)
 	`).Error; err != nil {
 		tx.Rollback()
-		return nil, err
+		return err
 	}
 
+	// Commit the transaction
 	if err := tx.Commit().Error; err != nil {
-		return nil, err
+		return err
 	}
 
-	return toBeSaved, nil
+	return nil
 }
